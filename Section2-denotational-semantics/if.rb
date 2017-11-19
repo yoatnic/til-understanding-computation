c5b077f614bdb4fb9e9acaf6aa47e2145be88e77
yoat <yoatnic@me.com>
2017-11-19 21:54:28 +0900

    Section 2.4.2-2. add if

---
+++ Section2-denotational-semantics/if.rb
@@ -0,0 +1,16 @@
+class If < Struct.new(:condition, :consequence, :alternative)
+  def to_s
+    "if (#{condition}) { #{consequence} } else { #{alternative} }"
+  end
+
+  def inspect
+    "<<#{self}>>"
+  end
+
+  def to_ruby
+    "-> e{ if (#{condtion.to_ruby}).call(e)" +
+      " then (#{consequence.to_ruby}).call(e)" +
+      " else (#{alternative}.to_ruby).call(e)" +
+      " end }"
+  end
+end
