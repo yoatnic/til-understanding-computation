638be05940bccf5e6e66cb54839d9178bb7ae908
yoat <yoatnic@me.com>
2017-11-19 21:55:36 +0900

    fix typo

--- Section2-denotational-semantics/if.rb
+++ Section2-denotational-semantics/if.rb
@@ -10,7 +10,7 @@ class If < Struct.new(:condition, :consequence, :alternative)
   def to_ruby
     "-> e{ if (#{condtion.to_ruby}).call(e)" +
       " then (#{consequence.to_ruby}).call(e)" +
-      " else (#{alternative}.to_ruby).call(e)" +
+      " else (#{alternative.to_ruby}).call(e)" +
       " end }"
   end
 end
