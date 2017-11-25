e23a0fa5a7797ee8172b5bcac863bcd134c3f921
yoat <yoatnic@me.com>
2017-11-25 20:55:38 +0900

    Section 3.2-2 add NFA

--- Section3-NFA/main.rb
+++ Section3-NFA/main.rb
@@ -1,6 +1,8 @@
 require 'set'
 require './NFARulebook.rb'
 require './FARule.rb'
+require './NFA'
+require './NFADesign'
 
 rulebook = NFARulebook.new([
   FARule.new(1, 'a', 1), FARule.new(1, 'b', 1), FARule.new(1, 'b', 2),
@@ -11,3 +13,28 @@ rulebook = NFARulebook.new([
 p rulebook.next_states(Set[1], 'b')
 p rulebook.next_states(Set[1, 2], 'b')
 p rulebook.next_states(Set[1, 3], 'b')
+
+puts '------------NFA------------'
+
+p NFA.new(Set[1], [4], rulebook).accepting?
+p NFA.new(Set[1, 2, 4], [4], rulebook).accepting?
+
+
+puts '------------read------------'
+nfa = NFA.new(Set[1], [4], rulebook);
+p nfa.accepting?
+nfa.read_character('b')
+p nfa.accepting?
+nfa.read_character('a')
+p nfa.accepting?
+nfa.read_character('b')
+p nfa.accepting?
+nfa.read_string('bbbb')
+p nfa.accepting?
+
+
+puts '------------factory------------'
+nfa_design = NFADesign.new(1, [4], rulebook)
+p nfa_design.accepts?('bab')
+p nfa_design.accepts?('bbbbb')
+p nfa_design.accepts?('bbbabb')
